80abedb6611e798eb5c46674f7df132aeeba3c5d
Phanidhar K <phanidhar.kammara@gmail.com>
2015-02-12 22:12:02 -0500

    initial

--- webapplication/src/main/java/org/bit/web/controller/WebController.java
+++ webapplication/src/main/java/org/bit/web/controller/WebController.java
@@ -1,10 +1,16 @@
-package com.bitmore.search.controller;
+package org.bit.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * @author phanidhar & leela
+ *
+ */
 
 @Controller
 @RequestMapping("/")
