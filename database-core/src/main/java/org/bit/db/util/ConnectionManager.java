29c26f4fa2057e547d15c27bb6acfb583a64407e
Phanidhar K <phanidhar.kammara@gmail.com>
2015-02-13 07:36:46 -0500

    initial database configuration

--- database-core/src/main/java/org/bit/db/util/ConnectionManager.java
+++ database-core/src/main/java/org/bit/db/util/ConnectionManager.java
@@ -1,5 +1,13 @@
 package org.bit.db.util;
 
-public class ConnectionManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class ConnectionManager {
+	public static void main(String[] args) throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testmaster", "root", "root");
+		System.out.println("Connection :"+con);
+		System.out.println("Connection :"+con.getMetaData().getDatabaseProductName());
+	}
 }
