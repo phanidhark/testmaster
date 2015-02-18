f032ba00bdc8ffcf1a63fbd275018715fda00639
Phanidhar K <phanidhar.kammara@gmail.com>
2015-02-17 20:09:56 -0500

    user table altered

--- schema/test_insert_data.sql
+++ schema/test_insert_data.sql
@@ -1,7 +1,35 @@
-INSERT INTO users(username,password,enabled)
-VALUES ('phanidhar','phanidhar', TRUE);
-INSERT INTO users(username,password,enabled)
-VALUES ('leela','leela', TRUE);
+INSERT INTO `testmaster`.`users`
+(`USERNAME`,
+`PASSWORD`,
+`FIRST_NAME`,
+`LAST_NAME`,
+`EMAIL`,
+`MOBILE`,
+`enabled`)
+VALUES
+('phanidhar',
+'phanidhar',
+'Phanidhar',
+'Kammara',
+'phanidhar.kammara@gmail.com',
+'9701144474',
+'1');
+INSERT INTO `testmaster`.`users`
+(`USERNAME`,
+`PASSWORD`,
+`FIRST_NAME`,
+`LAST_NAME`,
+`EMAIL`,
+`MOBILE`,
+`enabled`)
+VALUES
+('leela',
+'leela',
+'leela',
+'kumar',
+'leela.kumar@gmail.com',
+'9849098490',
+'1');
 
 INSERT INTO user_roles (username, ROLE)
 VALUES ('phanidhar', 'ROLE_USER');
