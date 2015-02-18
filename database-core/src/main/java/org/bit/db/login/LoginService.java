eb34a9d8544fa3775d4b72f7021756bb31afa685
Phanidhar K <phanidhar.kammara@gmail.com>
2015-02-17 22:54:29 -0500

    user jdbc

--- database-core/src/main/java/org/bit/db/login/LoginService.java
+++ database-core/src/main/java/org/bit/db/login/LoginService.java
@@ -1,25 +1,51 @@
 package org.bit.db.login;
 
-import org.bit.db.domain.UserProfile;
+import org.bit.db.domain.User;
+import org.bit.db.domain.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
 /**
  * 
  * @author phanidhar & leela
- *
+ * 
  */
+
+@Component
 public class LoginService {
-	
-	 public UserProfile getUserDetails(String userId){
-		 // TODO
-		 return new UserProfile();
-	 }
-	 
-	 public boolean changePassword(String userId, String newPassword){
-		 //TODO
-		 return false;
-	 }
-	 
-	 public boolean createUser(UserProfile user){
-		 // TODO
-		 return true;
-	 }
+
+	@Autowired
+	private JdbcTemplate jdbcTemplateObject;
+
+	public User getUserDetails(String username) {
+		String sql = "SELECT * FROM USERS WHERE USERNAME = ?";
+		User user = jdbcTemplateObject.queryForObject(sql,new Object[] { username }, new UserMapper());
+		return user;
+	}
+
+	public boolean changePassword(String username, String newPassword) {
+		User user = getUserDetails(username);
+		if(user!=null){
+			
+		}
+		return false;
+	}
+
+	public boolean updateProfile(User user){
+		return false;
+	}
+	public boolean createUser(User user) {
+		String sql ="INSERT INTO `testmaster`.`users` (`USERNAME`, `PASSWORD`, `FIRST_NAME`, `LAST_NAME`, `EMAIL`, `MOBILE`, `enabled`)"+
+				" VALUES (?,?,?,?,?,?,?)";
+		Object[] params = new Object[] { user.getUsername(),user.getPassword(), user.getFirstName(),user.getLastName(),
+				user.getEmail(),user.getMobile(),user.isEnabled()};
+		int count= jdbcTemplateObject.update( sql,params);
+		if(count>0){
+		return true;
+		}else{
+			return false;
+		}
+	}
+
 }
--- database-core/src/test/java/org/bit/db/login/LoginServiceTest.java
+++ database-core/src/test/java/org/bit/db/login/LoginServiceTest.java
@@ -1,7 +1,8 @@
 package org.bit.db.login;
-import static org.junit.Assert.assertFalse;
-
+import org.bit.db.domain.User;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * 
  * @author phanidhar & leela
@@ -15,14 +16,28 @@ public class LoginServiceTest {
 	@Test
 	public void testUserDetails(){
 		System.out.println("Start testUserDetails");
-		assertFalse(false);
+		ApplicationContext context = new ClassPathXmlApplicationContext("config/db-core-config-test.xml");
+		loginService = (LoginService)context.getBean("loginService");
+		System.out.println("loginService:"+loginService.getUserDetails("vaishu"));
 		System.out.println("Completed testUserDetails");
 	}
 	
-	@Test
+	//@Test
 	public void testCreateUser(){
-		System.out.println("Start testCreateUser");
-		assertFalse(false);
-		System.out.println("Completed testCreateUser");
+		System.out.println("Start testUserDetails");
+		ApplicationContext context = new ClassPathXmlApplicationContext("config/db-core-config-test.xml");
+		loginService = (LoginService)context.getBean("loginService");
+		User user = new User();
+		user.setUsername("vaishu");
+		user.setPassword("test");
+		user.setFirstName("Vaishnavi");
+		user.setLastName("Kammara");
+		user.setEmail("vaishu@test.com");
+		user.setMobile("1234567892");
+		user.setEnabled(true);
+		System.out.println("loginService:"+loginService.createUser(user));
+		System.out.println("Completed testUserDetails");
 	}
+	
+	
 }
